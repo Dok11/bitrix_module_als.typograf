2d763736a3f3701bb9bdc5a2d0c9e184d47cf7d7
Oleg Postoev <Dok11@users.noreply.github.com>
2017-03-13 15:47:28 +0300

    Добавил класс ALSTypografLight

--- include.php
+++ include.php
@@ -4,10 +4,10 @@ CModule::AddAutoloadClasses(
 	'als.typograf',
 	Array(
 		'ALSTypograf'		=> 'classes/general/typograf.php',							// Общий класс для работы с типографом
+		'ALSTypografLight'	=> 'classes/general/typograf_light.php',			// Простой типограф на php
 	)
 );
 
 if(ADMIN_SECTION == true) {
 	CUtil::InitJSCore(Array('jquery'));
-
 }
